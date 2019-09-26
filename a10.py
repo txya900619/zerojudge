8a30385d483acc1d9588af8b0cc7f92a1cf25bb1
txya900619 <wayne900619@gmail.com>
2019-09-27 01:27:26 +0800

    new

---
+++ a10.py
@@ -0,0 +1,32 @@
+while True:
+    try:
+        num = int(input())
+        str_pow = "^"
+        num2 = 2
+        count = 0
+        ans =""
+        count_2= 0
+        while num!=1:
+            if num%num2==0:
+                count+=1;
+                num=num/num2;
+                if num%num2==0:
+                    continue
+                else:
+                    count_2+= 1;
+                    if(count_2>1):
+                        ans += " * "
+                    if(count>=2):   
+                        ans += str(num2)
+                        ans += "^";
+                        ans += str(count)
+                    else:
+                        ans +=str(num2)
+                    count = 0
+               
+            else:
+                num2+=1
+        print(ans)
+        print("\n")
+    except:
+        break
