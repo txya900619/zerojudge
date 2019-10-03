b616f1b46a2080be99c1cfc56c2a780e6ea5844d
txya900619 <wayne900619@gmail.com>
2019-10-04 03:07:30 +0800

    3rd ac b938

--- b938.cpp
+++ b938.cpp
@@ -1,32 +1,25 @@
 #include<iostream>
 using namespace std;
-bool man_killed[1000001]={false};
+int behide[1000001];
+bool died[1000001] ;
 int main(){
-    ios_base::sync_with_stdio(false),cin.tie(NULL);
+    cin.sync_with_stdio(false),cin.tie(NULL);
     int mannum,killnum;
     cin>>mannum>>killnum;
+    for(int i=1;i<mannum;i++){
+        behide[i]=i+1;
+    }
     while(killnum--){
         int num2killbehide;
         cin>>num2killbehide;
-        if(man_killed[num2killbehide]||num2killbehide>=mannum||num2killbehide<=0){
+        if(behide[num2killbehide]==0||died[num2killbehide]){
             cout<<"0u0 ...... ?\n";
-            continue;
-        }
-        for(int i = num2killbehide+1;i<=mannum;i++){
-            if(i==mannum&&man_killed[mannum]){
-                cout<<"0u0 ...... ?\n";
-                break;
-            }
-            if(man_killed[i]){
-                continue;
-            }else{
-                man_killed[i] = true;
-                cout<<i<<endl;
-                break;
-            }
+        }else{
+            cout<<behide[num2killbehide]<<"\n";
+            died[behide[num2killbehide]]= true;
+            behide[num2killbehide]=behide[behide[num2killbehide]];
             
         }
-
     }
     return 0;
 }
